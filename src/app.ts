2e19ea00faa98ef112b67a49df60d72eb1da78d1
= <=>
2024-12-11 18:23:04 +0000

    add rate limiting to the API

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express, { Application } from "express";
+import { rateLimit, RateLimitRequestHandler } from "express-rate-limit";
 
 import { errorHandler } from "./middlewares/errorHandler";
 import weatherRoutes from "./routes/weather.route";
 
-const app: Application = express();
+const app: Application = express(),
+    limiter: RateLimitRequestHandler = rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        limit: 10, // Limit each ip to 100 request per window
+        standardHeaders: 'draft-7',
+        legacyHeaders: false,
+    });
+
 app.use(express.json());
+app.use(limiter);
 
 app.use('/weather', weatherRoutes);
 
